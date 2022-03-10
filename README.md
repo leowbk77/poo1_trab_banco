bd712075c82cd9383a5c0515f5bb29f44f973279
Leonardo <leonardo.mf@ufu.br>
2022-03-10 16:23:22 -0300

    teste projeto do intelliJ

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # poo1_trab_banco
+
 Trabalho de POO1 - Agencia Bancaria
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module trab.poo1_trab_banco {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens trab.poo1_trab_banco to javafx.fxml;
+    exports trab.poo1_trab_banco;
+}
