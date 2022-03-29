8112865ced6201d57285544188d52ee4bd93c0f5
Leonardo <leonardo.mf@ufu.br>
2022-03-29 16:52:58 -0300

    primeira tela

---
+++ src/main/java/trab/poo1_trab_banco/HomepageControl.java
@@ -0,0 +1,15 @@
+package trab.poo1_trab_banco;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HomepageControl {
+    @FXML
+    Button botao1;
+    @FXML
+    Button botao2;
+    @FXML
+    Button botao3;
+    @FXML
+    Button botao4;
+}
--- src/main/java/trab/poo1_trab_banco/Main.java
+++ src/main/java/trab/poo1_trab_banco/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Tela-teste.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("homepage.fxml"));
         Scene cena = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("UmTitulo");
         stage.setScene(cena);
