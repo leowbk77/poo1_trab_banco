cb8c40bdaea6bca3a7ef6eb4dbaf1270fdf2d873
RafaelC-99 <rafaelcardosocatuti@gmail.com>
2022-04-06 07:54:05 -0300

    criando classe banco

--- src/main/java/trab/poo1_trab_banco/models/Banco.java
+++ src/main/java/trab/poo1_trab_banco/models/Banco.java
@@ -1,5 +1,16 @@
 package trab.poo1_trab_banco.models;
 
 public class Banco {
-    
+    List<Agencia> agencias = new ArrayList<>();
+
+    public Banco() {
+    }
+
+    public void adicionarAgencia(Agencia agencia){
+        agencias.add(agencia);
+    }
+
+    public void removerAgencia(Agencia agencia){
+        agencias.remove(agencia);
+    }
 }
